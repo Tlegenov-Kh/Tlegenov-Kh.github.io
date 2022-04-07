7e5550908a35f15134a58fc39e73d39f32aca3e2
Khudaybergen <k.tlegenov@student.inha.uz>
2022-04-07 22:25:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 AOS.init();
-window.location.replace('https://spiritpromotion.me')
+window.location.assign('https://spiritpromotion.me')
 window.addEventListener('DOMContentLoaded',()=>{
     // // Loader------------------------>
     const body = document.querySelector('body');
